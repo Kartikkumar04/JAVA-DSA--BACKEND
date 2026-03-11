dcf737b7516721c8f81ed58c2e931d54c1d56f46
Kartik Kumar <kartikkumarxx4@gmail.com>
2026-03-11 22:41:17 +0530

    day1 java
    by apna collge

---
+++ Day01/FirstClass.java
@@ -0,0 +1,9 @@
+package Day01;
+class FirstClass{
+    public static void main(String[] args) {
+        //Output 3 ways
+        System.out.println("Hello World");
+        System.out.print("this is my first java program");
+        System.out.println("I am Kartik \n B. Tech CSE Student \n of 6th sem");
+    }
+}
---
+++ Day01/PrintPattern.java
@@ -0,0 +1,10 @@
+package Day01;
+
+public class PrintPattern {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
